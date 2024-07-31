334855106c5782b347cae035ff987d4584c592b4
FrancisAmenya <amenyafrancis@gmail.com>
2024-07-31 14:33:18 -0700

    9 - fix 1b

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,15 +3,14 @@ def max_integer(my_list=[]):
     # Check if the list is empty
     if len(my_list) == 0:
         return None
-    
+
     # Initialize the maximum value with the first element
     max_value = my_list[0]
-    
+
     # Iterate through the list starting from the second element
     for num in my_list[1:]:
         # If current number is greater than max_value, update max_value
         if num > max_value:
             max_value = num
-    
-    return max_value
 
+    return max_value
