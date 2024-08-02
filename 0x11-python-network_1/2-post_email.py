398d6661891e47d9711f68cc45e17ecea67ff6ec
FrancisAmenya <menyafrancis@gmail.com>
2024-08-02 00:04:07 -0700

    0x10 Pyton Network #1, fix 4a ,2a

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,6 +5,7 @@
 - takes email as a parameter
 - displays the body of the response
 """
+
 import sys
 import urllib.parse
 import urllib.request
@@ -16,5 +17,5 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    with urllib.request.urlopen(request) as resp:
+        print(resp.read().decode("utf-8"))
